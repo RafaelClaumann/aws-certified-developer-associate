7cd8dc7bbc111a7529001762e6328f6c40af4e34
RafaelClaumann <rafael_claumann@hotmail.com>
2022-03-28 09:18:20 -0300

    infra - 5. [HOL] Configure Routing
    aws_default_route_table
      - buscar a route table default da VPC
      - definir uma rota para o internet gateway
      - adicionar tags de identificacao
    
    aws_internet_gateway
      - acesso a internet para subnets publicas
    
    aws_route_table - private-route-table
      - tabela de roteamento privada
      - associada as subnets privadas
      - trafego direcionado para IPs locais
    
    aws_route_table_association
     - associacao subnet x route table

--- infra/main.tf
+++ infra/main.tf
@@ -11,6 +11,25 @@ resource "aws_vpc" "custom-vpc" {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/default_route_table
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/main_route_table_association
+# Note that the default route, mapping the VPC's CIDR block to "local", is created implicitly and cannot be specified.
+resource "aws_default_route_table" "custom-vpc-main-route-table" {
+  default_route_table_id = aws_vpc.custom-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom-vpc-igw.id
+  }
+
+  tags = {
+    Name    = "custom-vpc-main-route-table"
+    Course  = "AWS Certified Solutions Architect Professional SAP-C01 2022"
+    Session = "Advanced VPC"
+    Class   = "5. [HOL] Configure Routing"
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet#example-usage
 resource "aws_subnet" "public-us-east-1a" {
   vpc_id            = aws_vpc.custom-vpc.id
@@ -70,4 +89,40 @@ resource "aws_subnet" "private-us-east-1b" {
     Session = "Advanced VPC"
     Class   = "3. [HOL] Create a Custom VPC with Subnets"
   }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+resource "aws_internet_gateway" "custom-vpc-igw" {
+  vpc_id = aws_vpc.custom-vpc.id
+
+  tags = {
+    Name    = "custom-vpc-igw"
+    Course  = "AWS Certified Solutions Architect Professional SAP-C01 2022"
+    Session = "Advanced VPC"
+    Class   = "5. [HOL] Configure Routing"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+# Note that the default route, mapping the VPC's CIDR block to "local", is created implicitly and cannot be specified.
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.custom-vpc.id
+
+  tags = {
+    Name    = "private-route-table"
+    Course  = "AWS Certified Solutions Architect Professional SAP-C01 2022"
+    Session = "Advanced VPC"
+    Class   = "5. [HOL] Configure Routing"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "associate-private-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1a.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "associate-private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private-route-table.id
 }
